88e167e5b26b8d996453618b75f3a7d309a2c167
jinjint <jinjint@amazon.com>
2020-07-01 18:02:22 -0400

    delete some uncessary files

---
+++ plot_trials.py
@@ -0,0 +1,17 @@
+import numpy as np
+np.set_printoptions(precision = 4)
+from generatePvalue import *
+from toimport import *
+from generateHPY import *
+import sys
+
+
+### Import utilities for plotting
+from plotting import*
+from generateHPY import*
+from toimport import*
+
+
+def plot_results(FDRrange, pirange, muNrange, muArange, sigma, NUMHYP, num_runs):
+
+   pass
